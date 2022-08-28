3f1b3149b40a1e7b878778426a2eea3a3937ed10
IgorCreator <asigoria@gmail.com>
2022-08-27 21:27:34 -0400

    Dry code. Partial creation for student view

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,11 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: [:show, :edit, :update]
+
   def index
     @students = Student.all
   end
 
   def show
-    @student = Student.find(params[:id])
   end
 
   def new
@@ -12,30 +13,32 @@ class StudentsController < ApplicationController
   end
 
   def edit
-    @student = Student.find(params[:id])
   end
 
   def create
     @student = Student.create(student_params)
     if @student.save
       flash[:success] = "You successfully signed up"
-      redirect_to root_path
+      redirect_to @student
     else
       render 'new'
     end
   end
 
   def update
-    @student = Student.find(params[:id])
     if @student.update(student_params)
       flash[:success] = "Student information was updated successfully."
-      redirect_to root_path
+      redirect_to @student
     else
       render 'edit'
     end
   end
 
   private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
 
   def student_params
     params.require(:student).permit(:name, :email)
